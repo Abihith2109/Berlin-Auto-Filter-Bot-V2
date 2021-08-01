fd01a48d07fb305baa439650d399504109fc7aef
Abhijith2109 <83421731+Abihith2109@users.noreply.github.com>
2021-08-01 15:39:24 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = caption \n"<b>Powered by: @movie_matrix</b>",
            
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
