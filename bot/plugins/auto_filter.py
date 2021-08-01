d3a45965c0ede2677a97a17703f4eedd4033793d
Abhijith2109 <83421731+Abihith2109@users.noreply.github.com>
2021-08-01 08:51:18 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Request: <code>{query}</code>\n\n🔆Join Here:<br><b> @Movie_Matrix🔆 </b>",
+                text=f"Found {(len_results)} Results For Your Request: <code>{query}</code>\n\n<b>🔆Powered by:🔆</b>\n <a href="https://t.me/Movie_Matrix"><b>Movie Matrix 😉🍿</b></a> ",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
